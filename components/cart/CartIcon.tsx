9b14ad7ed7c6273602cefb44e221d701f409d2d3
sajidmahamud835 <sajidmahamud835@gmail.com>
2025-12-31 15:29:27 +0600

    fix: hide cart badge when cart is empty

--- components/cart/CartIcon.tsx
+++ components/cart/CartIcon.tsx
@@ -11,16 +11,12 @@ const CartIcon = () => {
   return (
     <Link href={"/cart"} className="group relative">
       <ShoppingBag className="group-hover:text-shop_light_green hoverEffect" />
-      {itemCount > 0 ? (
+      {itemCount > 0 && (
         <span
           className={`absolute -top-1 -right-1 bg-shop_btn_dark_green text-white rounded-full text-xs font-semibold flex items-center justify-center min-w-[14px] h-[14px] ${itemCount > 9 ? "px-1" : ""
             }`}
         >
           {displayCount}
-        </span>
-      ) : (
-        <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white rounded-full text-xs font-semibold flex items-center justify-center min-w-[14px]">
-          0
         </span>
       )}
     </Link>
